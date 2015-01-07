a91df0912678b9be5cbcca219009f685136c2c9e
Patryk Ściborek <patryk@sciborek.com>
2015-01-07 15:58:53 +0100

    Small cleanups

--- teryt/utils.py
+++ teryt/utils.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 import xml.etree.cElementTree as et
 
 def parse(stream):
@@ -10,11 +9,3 @@ def parse(stream):
             continue
         yield {x.get('name'):x.text.strip() if x.text else None for x in element}
 
-#for x in teryt_parse('/tmp/teryt/TERC.xml'):
-#for x in teryt_parse('/tmp/teryt/WMRODZ.xml'):
-#for x in teryt_parse('/tmp/teryt/SIMC.xml'):
-#for x in teryt_parse('/tmp/teryt/ULIC.xml'):
-#    print x
-#parse('/tmp/teryt/SIMC.xml')
-#parse('/tmp/teryt/ULIC.xml')
-#parse('/tmp/teryt/TERC.xml')
